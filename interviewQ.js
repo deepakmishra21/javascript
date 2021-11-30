64ad784a8adc62f097312ce3b78a5da28973b89c
Deepak Mishra <deepak.mishra@bold.com>
2021-12-01 01:35:06 +0530

    new code

--- interviewQ.js
+++ interviewQ.js
@@ -6,9 +6,11 @@
 //https://leetcode.com/problems/binary-search/
 //https://leetcode.com/problems/concatenation-of-array/
 //https://leetcode.com/problems/fibonacci-number/
-
+//https://leetcode.com/problems/find-peak-element/
 //https://leetcode.com/problems/binary-search/
 //https://leetcode.com/problems/search-insert-position/
+//https://leetcode.com/problems/permutations/
+//https://leetcode.com/problems/subsets/
 //Design clock in browser
 
 //let vs var
