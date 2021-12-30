84d241a588a8e57d06ab53cb9c54f64f5b660e28
mayankdhawan24 <mayankdhawan24@gmail.com>
2021-12-30 13:16:00 +0530

    3sum_Mayank.js

---
+++ 3sum_Mayank.js
@@ -0,0 +1,26 @@
+console.log(threeSum([0, 0, 0, 0]));
+
+function threeSum(nums) {
+  var result = [];
+  nums.sort((a, b) => { return a - b; });
+
+  for (let i = 0; i < nums.length; i++) {
+    var l = i + 1;
+    var r = nums.length - 1;
+    while (l < r) {
+      let sum = nums[i] + nums[l] + nums[r];
+      if (sum == 0) {
+        result.push([nums[i], nums[l], nums[r]]);
+        l++;
+        r--;
+      }
+      else if (sum < 0)
+        l++;
+      else if (sum > 0)
+        r--;
+
+    }
+  }
+  debugger;
+  return Array.from(new Set(result.map(JSON.stringify)), JSON.parse);
+}
