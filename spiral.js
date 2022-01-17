8b4e805d5367495963399bbebc0c9cc6a78253bb
Deepak Mishra <deepak.mishra@bold.com>
2022-01-17 22:31:27 +0530

    spiral.js

--- spiral.js
+++ spiral.js
@@ -1,65 +1,37 @@
 var spiralOrder = function (matrix) {
-  let right = matrix[0].length;
-  let bottom = matrix.length;
-  let left = 0;
-  let top = 0;
-  let iterator = true;
-  let out = [];
-  let r = 0;
-  let c = 0;
-  let dir = 'right';//down/left/up
-  while (iterator) {
-    if (!matrix[r] || matrix[r][c] == undefined || matrix[r][c] == 'seen') {
-      break;
-    }
-    if (dir == 'right') {
-      while (matrix[r][c] != 'seen' && c < right) {
-        out.push(matrix[r][c]);
-        matrix[r][c] = 'seen';
-        c++;
+  let size = matrix.length * matrix[0].length;
+  let result = [];
+  let col_start = 0, col_end = matrix[0].length - 1;
+  let row_start = 0, row_end = matrix.length - 1;
+  while (result.length < size) {
+    if (result.length < size) {
+      for (let i = col_start; i <= col_end; i++) {
+        result.push(matrix[row_start][i]);
       }
-      c--;
-      r++;
-      right--;
-      top++;
-      dir = 'down'
+      row_start += 1;
     }
-    else if (matrix[r][c] != 'seen' && dir == 'down') {
-      while (r < bottom) {
-        out.push(matrix[r][c]);
-        matrix[r][c] = 'seen';
-        r++;
+    if (result.length < size) {
+      for (let i = row_start; i <= row_end; i++) {
+        result.push(matrix[i][col_end]);
       }
-      r--;
-      c--;
-      bottom--;
-      dir = 'left'
+      col_end -= 1;
     }
-    else if (matrix[r][c] != 'seen' && dir == 'left') {
-      while (c >= left) {
-        out.push(matrix[r][c]);
-        matrix[r][c] = 'seen';
-        c--;
+    if (result.length < size) {
+      for (let i = col_end; i >= col_start; i--) {
+        result.push(matrix[row_end][i]);
       }
-      c++;
-      r--;
-      left++;
-      dir = 'up'
+      row_end -= 1;
     }
-    else if (matrix[r][c] != 'seen' && dir == 'up') {
-      while (r >= top) {
-        out.push(matrix[r][c]);
-        matrix[r][c] = 'seen';
-        r--;
+
+    if (result.length < size) {
+      for (let i = row_end; i >= row_start; i--) {
+        result.push(matrix[i][col_start]);
       }
-      r++;
-      c++;
-      top++;
-      dir = 'right'
+      col_start++;
     }
   }
-  console.log(matrix, out)
-  return out;
+  console.log(result);
+  return result;
 };
 spiralOrder([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]])
 spiralOrder([[1]]);
