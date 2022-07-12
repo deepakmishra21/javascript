9f08916b7212540739e09370d32a78c74968937c
Deepak Mishra <deepak.mishra@bold.com>
2022-07-12 17:12:31 +0530

    okoko

--- temp.js
+++ temp.js
@@ -1,11 +1,3 @@
 var rob = function (nums) {
-  var max = 0;
-  var prev = 0;
-  var curr = 0;
-  for (var i = 0; i < nums.length; i++) {
-    curr = Math.max(prev + nums[i], curr);
-    prev = max;
-    max = Math.max(max, curr);
-  }
-  return max;
+
 };
