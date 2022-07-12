cb0104d1bf1a4d56a0f328393f52ac90d716d31d
Deepak Mishra <deepak.mishra@bold.com>
2022-07-12 17:14:39 +0530

    ok

--- TwoSum.js
+++ TwoSum.js
@@ -1,27 +1,12 @@
-let nums = [3, 2, 4];
-console.log(TwoSum(nums, 6));
-
-function TwoSum(nums, k) {
-  let map = new Map();
-
+var twoSum = function (nums, target) {
+  let map = {};
   for (let i = 0; i < nums.length; i++) {
-    if (map.has(nums[i])) {
-      let val = [...map.get(nums[i]), i];
-      map.set(nums[i], val);
-    } else
-      map.set(nums[i], [i]);
-
+    let diff = target - nums[i];
+    if (map[diff] !== undefined) {
+      return [map[diff], i];
+    }
+    map[nums[i]] = i;
   }
-
-  for (let i = 0; i < nums.length; i++) {
-
-    if ((k - nums[i]) == nums[i]) {
-      if (map.get(nums[i]).length > 1)
-        return map.get(nums[i]);
-    } else if (map.has(k - nums[i]))
-      return [i, ...map.get(k - nums[i])];
-
-  }
-
-
-}
+  return [];
+};
+document.writeln(twoSum([1, 2, 3, 4, 5], 6));
